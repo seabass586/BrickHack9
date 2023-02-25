0ca543ad29b44fd564f18769919db56e3b5d360d
kchen5804 <kchen5804@bths.edu>
2023-02-25 12:33:39 -0500

    Added in camera opening and pressing 'q' to close

--- src/test.py
+++ src/test.py
@@ -1,6 +1,25 @@
 import cv2
+import numpy
+
+cap = cv2.VideoCapture(0)
+
+# Opens window to show camera capture, press q to quit
+while True:
+    ret, frame = cap.read()
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
 
 def main():
     print(cv2.__version__)
+
 
 main()
