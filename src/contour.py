26f3dc7c89118e8c8a8335fe2cecee3eda37a532
jwh1000 <jwh1000@rit.edu>
2023-02-25 14:38:55 -0500

    added canny algorithm and img dilation

--- src/contour.py
+++ src/contour.py
@@ -12,8 +12,8 @@ def empty(a):
 
 cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("threshold1", "Parameters", 150, 255, empty)
-cv2.createTrackbar("threshold1", "Parameters", 255, 255, empty)
+cv2.createTrackbar("Threshold1", "Parameters", 90, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 0, 255, empty)
 
 
 while True:
@@ -22,13 +22,19 @@ while True:
     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
 
+    # thresholds for the canny detection algorithm
     threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
     threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
 
+    # canny algorithm detects edges and shows the contours
     imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
 
+    # dilates the image so the edges are bigger and easier to see
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+
     # cv2.imshow("Result", img )
-    cv2.imshow("Result", imgCanny )
+    cv2.imshow("Result", imgDil )
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
