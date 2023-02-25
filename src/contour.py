51839ffec44fd6b29496b8f14afc15389ce8e715
jwh1000 <jwh1000@rit.edu>
2023-02-25 15:46:57 -0500

    can now differentiate between shapes

--- src/contour.py
+++ src/contour.py
@@ -12,12 +12,53 @@ def empty(a):
 
 cv2.namedWindow("Parameters")
 cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("Threshold1", "Parameters", 90, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 0, 255, empty)
+cv2.createTrackbar("Threshold1", "Parameters", 255, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 134, 255, empty)
+
+
+def getContours(img, imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, 
+                                           cv2.CHAIN_APPROX_NONE)
+
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+
+        if area > 5000:
+            cv2.drawContours(imgContour, cnt, -1, (0, 255, 0), 7)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+
+            x, y, w, h = cv2.boundingRect(approx)
+
+            match len(approx):
+                case 3:
+                   cv2.putText(imgContour, "Triangle", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
+                   break 
+                case 4:
+                   cv2.putText(imgContour, "Square", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
+                   break 
+                case 5:
+                   cv2.putText(imgContour, "Pentagon", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
+                   break 
+                case 6:
+                   cv2.putText(imgContour, "Hexagon", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
+                   break 
+                case 8:
+                   cv2.putText(imgContour, "Octagon", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
+                   break 
+
+        
 
 
 while True:
     success, img = cap.read()
+    imgContour = img.copy()
 
     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
@@ -33,8 +74,10 @@ while True:
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
+    getContours(imgDil, imgContour)
+
     # cv2.imshow("Result", img )
-    cv2.imshow("Result", imgDil )
+    cv2.imshow("Result", imgContour )
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
