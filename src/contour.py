97421d4fa224945dc942ea03a1a43d950b5c690d
jwh1000 <jwh1000@rit.edu>
2023-02-25 13:42:19 -0500

    trackbars

--- src/contour.py
+++ src/contour.py
@@ -7,12 +7,28 @@ cap = cv2.VideoCapture(0)
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 
+def empty(a):
+    pass
+
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("threshold1", "Parameters", 150, 255, empty)
+cv2.createTrackbar("threshold1", "Parameters", 255, 255, empty)
+
+
 while True:
     success, img = cap.read()
 
     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Result", imgGray )
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+
+    # cv2.imshow("Result", img )
+    cv2.imshow("Result", imgCanny )
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
