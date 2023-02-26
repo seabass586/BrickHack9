b2672c2429e42f14dcc58af51c591c28628fe986
seabass586 <supersebas369@gmail.com>
2023-02-26 05:23:49 -0500

    lose screen own file

--- src/start_menu.py
+++ src/start_menu.py
@@ -2,7 +2,6 @@ import pygame
 import sys
 import moviepy.editor
 import game 
-import mouse
 
 pygame.init() 
 pygame.display.set_caption("Shape It!")
@@ -153,49 +152,7 @@ def loading_screen():
     video2 = video.resize((1280, 720))
     video2.preview()  
 
-def lose_screen(points):
-    new_bg = pygame.image.load("assets/timesup.png")
-
-    while True:  
-        for ev in pygame.event.get():     
-            if ev.type == pygame.QUIT: 
-                pygame.quit()   
-            if ev.type == pygame.MOUSEBUTTONDOWN:
-                if 550 <= mouse[0] <= 610+140 and 415 <= mouse[1] <= 415+80: 
-                   start_screen()
-            if ev.type == pygame.MOUSEBUTTONDOWN: 
-                if 550 <= mouse[0] <= 610+140 and 535 <= mouse[1] <= 535+80: 
-                    pygame.quit()
-                    sys.exit()
-
-        text = bigfont.render("Points: " + points, True, WHITE)
-
-        mouse = pygame.mouse.get_pos() 
-        new = pygame.transform.scale(new_bg, (1280, 720))
-        screen.blit(new, (0, 0))
-
-        if 550 <= mouse[0] <= 600+140 and 415 <= mouse[1] <= 415+80: 
-            img = pygame.image.load('assets/home_selected.png')
-            img = pygame.transform.scale(img, (200, 90))
-            screen.blit(img,(545,415))
-        else: 
-            img = pygame.image.load('assets/home_button.png')
-            img = pygame.transform.scale(img, (200, 90))
-            screen.blit(img,(545,415))
-        if 550 <= mouse[0] <= 600+140 and 535 <= mouse[1] <= 535+80: 
-            img = pygame.image.load('assets/exit_selected.png')
-            img = pygame.transform.scale(img, (200, 90))
-            screen.blit(img,(545,535))
-        else: 
-            img = pygame.image.load('assets/exit_button.png')
-            img = pygame.transform.scale(img, (200, 90))
-            screen.blit(img,(545,535)) 
-
-        screen.blit(text,(500, 315))
-        
-        pygame.display.update() 
-
 def main():
-    lose_screen("100")
+    start_screen()
 
 main()
