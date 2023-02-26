bdc5f9765a0c6d71cac8b7a02b1bdc0558f60047
kchen5804 <kchen5804@bths.edu>
2023-02-25 19:53:49 -0500

    Added check() function

--- src/colors.py
+++ src/colors.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def main():
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
