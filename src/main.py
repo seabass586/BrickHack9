5a5f13256aa7786db1a2c27dac49a63923b2fa7d
jwh1000 <jwh1000@rit.edu>
2023-02-25 22:50:51 -0500

    movie

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame
 import cv2
 import numpy as np
+import random
 
 
 def empty(a):
@@ -25,32 +26,63 @@ def getContours(img, imgContour, shapeDict):
                 case 3:
                     cv2.putText(imgContour, "Triangle", (x + w + 20, y + 20), 
                             cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
-                    shapeDict['Triangle'] += 1
+                    shapeDict['triangle'] += 1
                     break 
                 case 4:
                     cv2.putText(imgContour, "Square", (x + w + 20, y + 20), 
                             cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
-                    shapeDict['Square'] += 1
+                    shapeDict['square'] += 1
                     break 
                 case 5:
                     cv2.putText(imgContour, "Pentagon", (x + w + 20, y + 20), 
                             cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
-                    shapeDict['Pentagon'] += 1
+                    shapeDict['pentagon'] += 1
                     break 
                 case 6:
                     cv2.putText(imgContour, "Hexagon", (x + w + 20, y + 20), 
                             cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
-                    shapeDict['Hexagon'] += 1
+                    shapeDict['hexagon'] += 1
                     break 
                 case 8:
                     cv2.putText(imgContour, "Octagon", (x + w + 20, y + 20), 
                             cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
-                    shapeDict['Octagon'] += 1
+                    shapeDict['octagon'] += 1
+                    break 
+                case _:
+                    cv2.putText(imgContour, "Unknown", (x + w + 20, y + 20), 
+                            cv2.FONT_HERSHEY_COMPLEX, .7, (0,255,0), 2)
                     break 
 
 
-def check(shapeDict):
-    pass
+def check(shapeDict, target):
+
+    for shape in shapeDict:
+        if shapeDict[shape] >= 50:
+            if shape == target:
+                return True
+            elif shapeDict[shape] >= 80 and shape != target:
+                return False
+
+
+def get_shape():
+    val = random.randint(0,4)
+    if val == 0:
+        return 'triangle'
+    elif val == 1:
+        return 'square'
+    elif val == 2:
+        return 'pentagon'
+    elif val == 3:
+        return 'hexagon'
+    elif val == 4:
+        return 'octagon'
+    
+
+def clear_dict(shapeDict):
+    for shape in shapeDict:
+        shapeDict[shape] = 0
+    print(shapeDict)
+    return shapeDict
 
 
 def game_loop(webcam):
@@ -63,20 +95,24 @@ def game_loop(webcam):
     fps = 30
     clock = pygame.time.Clock()
 
-    shapeDict = {'Triangle': 0, 'Square': 0, 'Pentagon': 0, 
-                     'Hexagon': 0, 'Octagon': 0}
+    shapeDict = {'triangle': 0, 'square': 0, 'pentagon': 0, 
+                 'hexagon': 0, 'octagon': 0}
 
     start = True
+    drawing = True
+    task = get_shape()
+    print(task)
+
     while start:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 start = False
                 pygame.quit()
 
-
         # OpenCV
         success, img = webcam.read()
         imgContour = img.copy()
+
 
         imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
         imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
@@ -94,7 +130,17 @@ def game_loop(webcam):
 
         getContours(imgDil, imgContour, shapeDict)
 
-        check(shapeDict)
+        if check(shapeDict, task) == True:
+            print ("Nice!!")
+            shapeDict = clear_dict(shapeDict)
+            task = get_shape()
+            print(task)
+        elif check(shapeDict, task) == False:
+            print ("NO!!!!! >:(")
+            shapeDict = clear_dict(shapeDict)
+            task = get_shape()
+            print(task)
+
 
         imgContour = cv2.cvtColor(imgContour.copy(), cv2.COLOR_BGR2RGB)
         imgContour = np.rot90(imgContour) 
@@ -102,7 +148,7 @@ def game_loop(webcam):
         frame = pygame.surfarray.make_surface(imgContour).convert()
         frame = pygame.transform.flip(frame, True, False)
 
-        window.blit(frame, (0,0))
+        window.blit(frame, (155, 90))
 
         pygame.display.update()
 
@@ -110,8 +156,8 @@ def game_loop(webcam):
 
 
 def main():
-    frameWidth = 1280
-    frameHeight = 720
+    frameWidth = 1180
+    frameHeight = 620
     cap = cv2.VideoCapture(0)
     cap.set(3, frameWidth)
     cap.set(4, frameHeight)
