f1d846dbe589c2a60d6dd02aa01e5126c54b1aaf
seabass586 <supersebas369@gmail.com>
2023-02-25 19:23:43 -0500

    redesigned menu

--- src/start_menu.py
+++ src/start_menu.py
@@ -1,59 +1,63 @@
-from time import sleep
-import pygame
-import pygame_menu
-from pygame_menu import themes
-
-pygame.init()
+import pygame 
+import sys
+  
+pygame.init() 
 pygame.display.set_caption("Shape It!")
-surface = pygame.display.set_mode()
-width, height = pygame.display.get_surface().get_size()
-print(width, height)
+bg = pygame.image.load("assets/placeholder.jpg")
 
-def difficulty(value, difficulty):
-    print(value)
-    print(difficulty)
+WHITE = (255,255,255) 
+GREY = (170,170,170)
+DARK_GREY = (100,100,100)
 
-def start():
-    pass
+res = 1280, 720
+screen = pygame.display.set_mode(res) 
+smallfont = pygame.font.SysFont('Comics Sans MS',40) 
+text1 = smallfont.render("Start", True, WHITE)
+text2 = smallfont.render("Mode", True, WHITE)
+text3 = smallfont.render('Quit' , True , WHITE) 
 
-def level_menu():
-    mainmenu._open(level)
+def start_screen():  
+    while True:  
+        for ev in pygame.event.get():     
+            if ev.type == pygame.QUIT: 
+                pygame.quit()   
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if 610 <= mouse[0] <= 610+140 and 425 <= mouse[1] <= 490: 
+                    print("Start game")
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if 610 <= mouse[0] <= 610+140 and 535 <= mouse[1] <= 590: 
+                    print("Options game")
+            if ev.type == pygame.MOUSEBUTTONDOWN: 
+                if 610 <= mouse[0] <= 610+140 and 575 <= mouse[1] <= 650: 
+                    pygame.quit()
+                    sys.exit()
 
-#adds basic menu layout
-mainmenu = pygame_menu.Menu('WELCOME TO SHAPE IT!', width, height, theme=themes.THEME_GREEN)
-mainmenu.add.text_input('Name: ', default='insert name here', maxchar=20)
-mainmenu.add.button('Play', start)
-mainmenu.add.button('Select Difficulty', level_menu)
-mainmenu.add.button('Quit', pygame_menu.events.EXIT)
- 
-#lets you change the difficuty
-level = pygame_menu.Menu('Select a Difficulty', width, height, theme=themes.THEME_BLUE)
-level.add.selector('Difficulty :', [('Learning', 1), ('Challenge', 2)], onchange=difficulty)
+        mouse = pygame.mouse.get_pos() 
+        new = pygame.transform.scale(bg, (1280, 720))
+        screen.blit(new, (0, 0))
 
-#loading bar
-loading = pygame_menu.Menu('Loading...', width, height, theme=themes.THEME_DARK)
-loading.add.progress_bar("Progress", progressbar_id = "1", default=0, width = 200, )
- 
-arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (10, 15))
- 
-update_loading = pygame.USEREVENT + 0
- 
-#loading loop
-while True:
-    events = pygame.event.get()
-    for event in events:
-        if event.type == update_loading:
-            progress = loading.get_widget("1")
-            progress.set_value(progress.get_value() + 1)
-            if progress.get_value() == 100:
-                pygame.time.set_timer(update_loading, 0)
-        if event.type == pygame.QUIT:
-            exit()
- 
-    if mainmenu.is_enabled():
-        mainmenu.update(events)
-        mainmenu.draw(surface)
-        if (mainmenu.get_current().get_selected_widget()):
-            arrow.draw(surface, mainmenu.get_current().get_selected_widget())
- 
-    pygame.display.update()
+        if 610 <= mouse[0] <= 610+140 and 450 <= mouse[1] <= 450+40: 
+            pygame.draw.rect(screen, GREY,[575,440,140,40]) 
+        else: 
+            pygame.draw.rect(screen, DARK_GREY, [575,440,140,40]) 
+
+        if 610 <= mouse[0] <= 610+140 and 550 <= mouse[1] <= 550+40: 
+            pygame.draw.rect(screen, GREY,[575,535,140,40]) 
+        else: 
+            pygame.draw.rect(screen,DARK_GREY,[575,535,140,40]) 
+
+        if 610 <= mouse[0] <= 610+140 and 625 <= mouse[1] <= 650+40: 
+            pygame.draw.rect(screen, GREY,[575,615,140,40]) 
+        else: 
+            pygame.draw.rect(screen,DARK_GREY,[575,615,140,40]) 
+
+        screen.blit(text1, (610, 445)) 
+        screen.blit(text2, (610, 545)) 
+        screen.blit(text3, (610, 625)) 
+
+        pygame.display.update() 
+
+def main():
+    start_screen()
+
+main()
